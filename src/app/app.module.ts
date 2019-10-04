9a69244e3119e5f0e0f5f77215a433ec4b860ff6
chuckderosier <chuckderosier2018@gmail.com>
2019-10-04 14:34:30 -0400

    made a custom directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,42 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+// FOR SERVER 2
+import { AppRoutingModule } from './app-routing.module';
 import { Server2Component } from './server2/server2.component';
 
+// FOR CUSTOM DIRECTIVE
+import { CustomDirectiveStylesDirective } from './custom-directive-styles/custom-directive-styles.directive';
+
+// FOR CUSTOM DIRECTIVE
 @NgModule({
   declarations: [
     AppComponent,
-    Server2Component
+    Server2Component, // server 2
+    CustomDirectiveStylesDirective  // custom directive
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule // server 2
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// FOR SERVER 2 COMPONENT
+
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     Server2Component
+//   ],
+//   imports: [
+//     BrowserModule,
+//     AppRoutingModule
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
