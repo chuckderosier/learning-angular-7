7c2416653e46c6b80364c828cd392d6463bd6ed1
chuckderosier <chuckderosier2018@gmail.com>
2019-10-09 15:05:00 -0400

    creted Server2Component class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+
+// export class AppComponent {
+//   title = 'seven-angular';
+//   servers = [];   // If not declared an array push will come back undefined
+//   OnAddServer() {
+//     this.servers.push('Another Server Added');
+//   }
+//   onRemoveServer(id: number) {
+//     const position = id + 1;
+//     this.servers.splice(position, 1);
+//   }
+// }
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-sever-2',
+  templateUrl: './server2/server2.component.html',
+  styleUrls: ['./server2/server2.component.css']
 })
-export class AppComponent {
-  title = 'seven-angular';
-  servers = [];   // If not declared an array push will come back undefined
-  OnAddServer() {
-    this.servers.push('Another Server Added');
+export class Server2Component {
+  allowNewServer = false;
+  serverCreationStatus = 'No server is created';
+  serverName = 'TestServer';
+  serverCreated = false;
+  /*constructor() {
+    setTimeout (() => {
+      this.allwNewServer = true;
+    }, 5000);
+  }*/
+  ngOnInit () {
   }
-  onRemoveServer(id: number) {
-    const position = id + 1;
-    this.servers.splice(position, 1);
+  onCreateServer() {
+    this.serverCreated = true;
+    this.serverCreationStatus = 'Server is created. Name: ' + this.serverName;
+  }
+  OnUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 }
-
