0e936660eeaf996852c487251aadda534a122a52
chuckderosier <chuckderosier2018@gmail.com>
2019-10-09 15:09:51 -0400

    move directive lesson to seperate app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,44 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 
-// @Component({
-//   selector: 'app-root',
-//   templateUrl: './app.component.html',
-//   styleUrls: ['./app.component.css']
-// })
-
-// export class AppComponent {
-//   title = 'seven-angular';
-//   servers = [];   // If not declared an array push will come back undefined
-//   OnAddServer() {
-//     this.servers.push('Another Server Added');
-//   }
-//   onRemoveServer(id: number) {
-//     const position = id + 1;
-//     this.servers.splice(position, 1);
-//   }
-// }
 @Component({
-  selector: 'app-sever-2',
-  templateUrl: './server2/server2.component.html',
-  styleUrls: ['./server2/server2.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class Server2Component {
-  allowNewServer = false;
-  serverCreationStatus = 'No server is created';
-  serverName = 'TestServer';
-  serverCreated = false;
+
+export class AppComponent {
+  title = 'seven-angular';
+  servers = [];   // If not declared an array push will come back undefined
+  OnAddServer() {
+    this.servers.push('Another Server Added');
+  }
+  onRemoveServer(id: number) {
+    const position = id + 1;
+    this.servers.splice(position, 1);
+  }
+}
+// @Component({
+//   selector: 'app-sever-2',
+//   templateUrl: './server2/server2.component.html',
+//   styleUrls: ['./server2/server2.component.css']
+// })
+// export class Server2Component {
+//   allowNewServer = false;
+//   serverCreationStatus = 'No server is created';
+//   serverName = 'TestServer';
+//   serverCreated = false;
   /*constructor() {
     setTimeout (() => {
       this.allwNewServer = true;
     }, 5000);
   }*/
-  ngOnInit () {
-  }
-  onCreateServer() {
-    this.serverCreated = true;
-    this.serverCreationStatus = 'Server is created. Name: ' + this.serverName;
-  }
-  OnUpdateServerName(event: Event) {
-    this.serverName = (<HTMLInputElement>event.target).value;
-  }
-}
+//   ngOnInit () {
+//   }
+//   onCreateServer() {
+//     this.serverCreated = true;
+//     this.serverCreationStatus = 'Server is created. Name: ' + this.serverName;
+//   }
+//   OnUpdateServerName(event: Event) {
+//     this.serverName = (<HTMLInputElement>event.target).value;
+//   }
+// }
